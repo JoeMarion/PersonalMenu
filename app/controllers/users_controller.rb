60424871aec94319aa635835ce2c70e5dba4dd42
= <joeyzmarion@gmail.com>
2016-05-17 23:51:34 -0500

    Finish log in & log out

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -10,6 +10,7 @@ class UsersController < ApplicationController
   def create
     @user = User.new(user_params)
     if @user.save
+      log_in @user
       flash[:success] = "Welcome to Personal Menu Maker!"
       redirect_to @user
     else
---
+++ test/integration/users_login_test.rb
@@ -0,0 +1,43 @@
+require 'test_helper'
+
+class UsersLoginTest < ActionDispatch::IntegrationTest
+
+  def setup
+    @user = users(:joe)
+  end
+
+  test "login with invalid information" do
+    get login_path
+    assert_template 'sessions/new'
+    post login_path, session: { email: "", password: "" }
+    assert_template 'sessions/new'
+    assert_not flash.empty?
+    get root_path
+    assert flash.empty?
+  end
+
+  test "login with valid information followed by logout" do
+    get login_path
+    post login_path, session: { email: @user.email, password: 'password' }
+    assert_redirected_to @user
+    follow_redirect!
+    assert_template 'users/show'
+    # test for changes in links to show logged in users
+    delete logout_path
+    assert_not is_logged_in?
+    assert_redirected_to root_url
+    delete logout_path
+    follow_redirect!
+    # test for link changes here too
+  end
+
+  test "login with remembering" do
+    log_in_as(@user, remember_me: '1')
+    assert_not_nil cookies['remember_token']
+  end
+
+  test "login without remembering" do
+    log_in_as(@user, remember_me: '0')
+    assert_nil cookies['remember_token']
+  end
+end
