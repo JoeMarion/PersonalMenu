5c1d2c53f4bf3b9803928cc02e42f902ffa6e3e1
= <joeyzmarion@gmail.com>
2016-05-17 19:32:37 -0500

    Use SSL and the Puma webserver in production

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,8 @@
 Rails.application.routes.draw do
-  get 'users/show'
-
-  get 'users/new'
-
-  get 'static_pages/home'
-
   root 'static_pages#home'
 
+  get 'signup', to: "users#new"
+
   resources :users
+
 end
